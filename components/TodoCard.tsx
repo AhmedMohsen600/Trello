87cbbd902f7f6d3bb3faa7df0c75d4bbd9369f68
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2023-06-04 23:33:22 +0300

    fix type erorr on TodoCard component

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,4 +1,25 @@
-function TodoCard() {
+import {
+  DraggableProvidedDraggableProps,
+  DraggableProvidedDragHandleProps,
+} from 'react-beautiful-dnd';
+
+// -------------------------------------------------------
+interface TodoCardProps {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps?: DraggableProvidedDragHandleProps | null;
+}
+
+function TodoCard({
+  todo,
+  index,
+  innerRef,
+  dragHandleProps,
+  draggableProps,
+}: TodoCardProps) {
   return <div>TodoCard</div>;
 }
 
